dc843cf35e47157be85475909ff89a588b9263c8
long1230 <1032629284@qq.com>
2019-04-13 19:22:30 +0800

    11

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.administrator.myapplication;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
+import android.view.ViewTreeObserver;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.tv);
+        textView.setText("这篇文章主要介绍了在Android TextView中显示图片的4种方式,需要的朋友可以参考下"+"asdasdsadsa");
+    }
+
+    public void toggleEllipsize(final Context context,
+                                final TextView textView,
+                                final int minLines,
+                                final String originText,
+                                final String endText,
+                                final int endColorID,
+                                final boolean isExpand) {
+        if (TextUtils.isEmpty(originText)) {
+            return;
+        }
+        textView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver
+                .OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (isExpand) {
+                    textView.setText(originText);
+                } else {
+                    int paddingLeft = textView.getPaddingLeft();
+                    int paddingRight = textView.getPaddingRight();
+                    TextPaint paint = textView.getPaint();
+                    float moreText = textView.getTextSize() * endText.length();
+                    float availableTextWidth = (textView.getWidth() - paddingLeft - paddingRight) *
+                            minLines - moreText;
+                    CharSequence ellipsizeStr = TextUtils.ellipsize(originText, paint,
+                            availableTextWidth, TextUtils.TruncateAt.END);
+                    if (ellipsizeStr.length() < originText.length()) {
+                        CharSequence temp = ellipsizeStr + endText;
+                        SpannableStringBuilder ssb = new SpannableStringBuilder(temp);
+                        ssb.setSpan(new ForegroundColorSpan(context.getResources().getColor
+                                        (endColorID)),
+                                temp.length() - endText.length(), temp.length(),    Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+                        textView.setText(ssb);
+                    } else {
+                        textView.setText(originText);
+                    }
+                }
+                if (Build.VERSION.SDK_INT >= 16) {
+                    textView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                } else {
+                    textView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+            }
+        });
+    }
+}
